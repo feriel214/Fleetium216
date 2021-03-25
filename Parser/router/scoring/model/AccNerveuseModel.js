3c51d92bb19aa186eb9b3a04d8aacd21728d8c60
minercool <zghidig@gmail.com>
2021-03-25 21:33:43 +0100

    messageerrorr

---
+++ Parser/router/scoring/model/AccNerveuseModel.js
@@ -0,0 +1,28 @@
+try {
+    var azure = require("azure-storage");
+    var connectionString =
+      "DefaultEndpointsProtocol=https;AccountName=pfe2021;AccountKey=4MudxJfKGSTpZBFzu8AozK9x47mGpvsFOdF2iPnobcJTRlOd7X7jwSFFvppr4atXQoQL07upQHbBzZhd37xBNg==;EndpointSuffix=core.windows.net";
+    var tableSvc = azure.createTableService(connectionString);
+} catch (error) {
+    console.log("can not connect to azure table storage");
+}
+
+function acceleration_threshold1_exceed(carId){
+    
+    PartitionKey = carId + "_" + "acceleration_threshold1_exceed";
+     query = new azure.TableQuery()
+    .select(['*'])
+    .where('PartitionKey eq?', PartitionKey);
+     tableSvc.queryEntities('eventsdata',query,null,  function (error, result){
+        
+            if(!error){
+               console.log(result.entries[0].obd_distance_travled_value._); 
+            }else{
+                console.log("Something went wrong");
+            }
+          });
+}
+
+module.exports = {
+    acceleration_threshold1_exceed
+}
