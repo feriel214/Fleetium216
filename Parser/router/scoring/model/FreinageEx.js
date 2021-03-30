8129a7db7cd770a8d8472dd367172f6afb44254f
minercool <zghidig@gmail.com>
2021-03-30 14:52:31 +0100

    newModel

---
+++ Parser/router/scoring/model/FreinageEx.js
@@ -0,0 +1,36 @@
+const { TableQuery } = require("azure-storage");
+try {
+    var azure = require("azure-storage");
+    var connectionString =
+      "DefaultEndpointsProtocol=https;AccountName=pfe2021;AccountKey=4MudxJfKGSTpZBFzu8AozK9x47mGpvsFOdF2iPnobcJTRlOd7X7jwSFFvppr4atXQoQL07upQHbBzZhd37xBNg==;EndpointSuffix=core.windows.net";
+    var tableSvc = azure.createTableService(connectionString);
+  } catch (error) {
+    console.log("can not connect to azure table storage");
+  }
+
+
+async function deceleration_threshold1_exceed(carId){
+    PartitionKey = carId + "_" + "deceleration_threshold1_exceed";
+    return new Promise((resolve, reject) => {
+    query = new azure.TableQuery()
+    .select(['*'])
+    .where('PartitionKey eq?', PartitionKey);
+   tableSvc.queryEntities('eventsdata',query,null, function (error, result){
+          if(!error){
+            if(result.entries.length != 0){
+              resolve(result.entries.length);
+            }else{
+              resolve(NaN)
+            }
+                
+          }else{
+              reject(error);
+          }
+        });
+      });
+}
+
+
+module.exports = { 
+    deceleration_threshold1_exceed
+}
