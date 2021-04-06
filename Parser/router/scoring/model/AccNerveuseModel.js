a8b2dfd6b165e5b3e102e107ebaa41995cf9d49b
minercool <zghidig@gmail.com>
2021-04-06 15:42:47 +0100

    continuationToken

--- Parser/router/scoring/model/AccNerveuseModel.js
+++ Parser/router/scoring/model/AccNerveuseModel.js
@@ -8,29 +8,40 @@ try {
 } catch (error) {
   console.log("can not connect to azure table storage");
 }
-
-
-
- async function acceleration_threshold1_exceed(carId){
-  PartitionKey = carId + "_" + "acceleration_threshold1_exceed";
-  return new Promise((resolve, reject) => {
-   query = new azure.TableQuery()
-  .select(['*'])
-  .where('PartitionKey eq?', PartitionKey);
-   tableSvc.queryEntities('eventsdata',query,null, function (error, result){
-          if(!error){
-            if(result.entries.length != 0){
-              resolve(result.entries.length);
+    async function queryEntitiesSegmented(carId , continuationToken){
+      PartitionKey = carId + "_" + "acceleration_threshold1_exceed";
+      return new Promise((resolve,reject)=>{
+        query = new azure.TableQuery()
+        .select(['*'])
+        .where('PartitionKey eq?', PartitionKey);
+          tableSvc.queryEntities('eventsdata', query, continuationToken, (error, results)=> {
+            if(!error){
+              if(results.entries.length != 0){
+                resolve(results);
+              }else{
+                reject(NaN);
+              }    
             }else{
-              reject(NaN);
-            }    
-          }else{
-              reject(error);
-          }
-        });
+                reject(error);
+            }
+          });
       });
-    }
-
+  }
+  
+  async function acceleration_threshold1_exceed(carId){
+      var tableQuery = new azure.TableQuery();
+      
+      var continuationToken = null;
+      var entities = [];
+      do{
+          var results =  await queryEntitiesSegmented(carId, continuationToken);
+          continuationToken = results.continuationToken;
+          
+          
+      }
+      while(continuationToken!=null);
+      return results.entries.length;
+  }
 
 module.exports = {
   acceleration_threshold1_exceed
