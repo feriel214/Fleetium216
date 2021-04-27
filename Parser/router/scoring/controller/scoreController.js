abaec9655efbfd76634e5d2dd6a5d9cc594c3ced
minercool <zghidig@gmail.com>
2021-04-27 19:51:19 +0100

    update

--- Parser/router/scoring/controller/scoreController.js
+++ Parser/router/scoring/controller/scoreController.js
@@ -1,11 +1,11 @@
-const scoreModel = require('../model/finalScoreModel');
+const finaScoreModel = require('../model/finalScoreModel');
 const getAllScores = require('../model/scoresModel');
 const { json } = require('body-parser');
 
 module.exports = {
     getScore : async function(req,res){
         try {
-            result = await scoreModel.calcScore(req.body.carId,req.body.debut,req.body.fin);
+            result = await finaScoreModel.calcScore(req.body.carId,req.body.debut,req.body.fin);
             if(result != null){
                 res.status(200).json(result);
             }else{
