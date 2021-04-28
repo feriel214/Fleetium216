2e02300cb3180e79a3605109990b83ad50199402
minercool <zghidig@gmail.com>
2021-04-28 16:11:24 +0100

    RoadSpeedUpdates

--- Parser/router/scoring/model/RoadSpeedModel.js
+++ Parser/router/scoring/model/RoadSpeedModel.js
@@ -1,11 +1,6 @@
   async function RoadSpeed(dt){
-      let max1 = 0;
-      let min1 = 9999999999999;
-      let max2 = 0;
-      let min2 = 9999999999999;
-      let max3 = 0;
-      let min3 = 9999999999999;
-      var speed = {
+    let j =0  
+    var speed = {
           speed_1 : 0,
           speed_2 : 0,
           speed_3 : 0
@@ -13,47 +8,22 @@
       for (i = 0; i < dt.entries.length ; i++)
             {
                 if(parseInt(dt.entries[i].gps_speed._) > 0 && parseInt(dt.entries[i].gps_speed._) <= 90){
-                    if (max1 < parseInt(dt.entries[i].RowKey._)){
-                        max1 = parseInt(dt.entries[i].RowKey._)
-                    }
-                    if (min1 > parseInt(dt.entries[i].RowKey._)){
-                        min1 = parseInt(dt.entries[i].RowKey._)
-                    }
+                    j++;
+                    speed.speed_1 = speed.speed_1 + 5;
                 }else
                 if(parseInt(dt.entries[i].gps_speed._) > 90 && parseInt(dt.entries[i].gps_speed._) <= 120)
                 {
-                    if (max2 < parseInt(dt.entries[i].RowKey._)){
-                        max2 = parseInt(dt.entries[i].RowKey._)
-                    }
-                    if (min2 > parseInt(dt.entries[i].RowKey._)){
-                        min2 = parseInt(dt.entries[i].RowKey._)
-                    }
+                    speed.speed_2 = speed.speed_2 + 5;
                 }else
                 if(parseInt(dt.entries[i].gps_speed._) > 120)
                 {
-                    console.log(dt.entries[i].RowKey._)
-                    if (max3 < parseInt(dt.entries[i].RowKey._)){
-                        max3 = parseInt(dt.entries[i].RowKey._)
-                    }
-                    if (min3 > parseInt(dt.entries[i].RowKey._)){
-                        min3 = parseInt(dt.entries[i].RowKey._)
-                    }
+                    speed.speed_3 = speed.speed_3 + 5;
                 }
             }
-            speed.speed_1 = Math.round((max1 - min1)/60000)
-            speed.speed_2 = Math.round((max2 - min2)/60000)
-            speed.speed_3 = Math.round((max3 - min3)/60000)
-
-            if (speed.speed_2 < 0){
-                speed.speed_2 = 0
-            }
-            if (speed.speed_3 < 0){
-                speed.speed_3 = 0
-            }
-
-            
-       
-            return speed;
+            speed.speed_1 = Math.round(speed.speed_1 / 60);
+            speed.speed_2 = Math.round(speed.speed_2 / 60);
+            speed.speed_3 = Math.round(speed.speed_3 / 60);
+           return speed;
   }
 
  module.exports = {
